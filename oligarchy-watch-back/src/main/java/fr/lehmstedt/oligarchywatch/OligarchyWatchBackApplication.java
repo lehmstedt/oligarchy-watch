d543df71ec02530bfba1bfb6d9b313937779edef
Sébastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2018-11-17 16:02:56 +0100

    Démarre un projet spring boot

---
+++ oligarchy-watch-back/src/main/java/fr/lehmstedt/oligarchywatch/OligarchyWatchBackApplication.java
@@ -0,0 +1,12 @@
+package fr.lehmstedt.oligarchywatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OligarchyWatchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OligarchyWatchBackApplication.class, args);
+	}
+}
